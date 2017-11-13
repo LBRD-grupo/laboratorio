c415b848602e695f21690c3b6a09ecb454f4c7be
juan acosta <juangabrielacosta@outlook.com>
2017-11-13 18:02:32 -0300

    8: clase calculadora
    Task-Url: http://localhost/redmine/issues/8

---
+++ Calculadora/src/calculator/operacion.java
@@ -0,0 +1,21 @@
+package calculator;
+
+public  class   operacion {
+	 double n1;
+	    double n2;
+	    double res;
+	    char op;
+	public operacion(double n1, double n2, char operacion) {
+		this.n1 = n1;
+        this.n2 = n2;
+        this.op = operacion;
+	}
+        public void mostrarResultado(){
+            
+            System.out.println(this.n1+" "+this.op+" "+this.n2+" = "+this.res);
+            
+        }
+		
+	}
+
+
