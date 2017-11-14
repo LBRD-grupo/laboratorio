279f66c52d344535b588ac41aff486bb741ac064
markososa23 <markososa23@gmail.com>
2017-11-14 08:48:16 -0300

    date +"%d/%m/%Y+date +"%H:%M"+MENSAJE GENERADO AUTOMATICAMENTE

--- Calculadora/autogit.sh
+++ Calculadora/autogit.sh
@@ -28,7 +28,83 @@ else
 	git commit -m "$DIA+$HORA+$TXT"
 
 	# Y terminamos subiendo los archivos
-	git push origin master
+	git push #!/bin/bash
+
+#autogit 0.1
+DIA='date +"%d/%m/%Y'
+HORA='date +"%H:%M"'
+TXT='MENSAJE GENERADO AUTOMATICAMENTE'
+# Modo de uso: copia o mueve este script a /usr/bin o /usr/local/bin y desde el directorio donde se encuentre la copia de un repo git, ejecútalo de esta manera:
+# autogit <ficheros>
+
+# Comprobamos si el directorio en el que estamos es de un repositorio git
+if [ ! -d '.git' ]; then
+	echo 'Esta carpeta no contiene un repositorio Git'
+	exit -1
+fi
+
+# Ahora comprobamos si se le paso algun parametro
+if [ $# == 0 ]; then
+	echo "autogit: ¡Error! No se le a pasado ningún parámetro"
+	exit -1
+else
+	# Recorremos los parametros para comprobar si son ficheros o directorios
+	for file in $*; do
+		if [ ! -e $file ]; then
+			echo "UpToGit: El archivo o directorio $file no existe"
+			exit -1
+		fi
+	done
+	
+	# Si llegamos hasta aquí, indicamos a Git los archivos a subir
+	git add $*
+	
+	# Esto nos pedira el mensaje del commit
+	git commit -m "$DIA+$HORA+$TXT"
+
+	# Y terminamos subiendo los archivos
+	git push #!/bin/bash
+
+#autogit 0.1
+DIA='date +"%d/%m/%Y'
+HORA='date +"%H:%M"'
+TXT='MENSAJE GENERADO AUTOMATICAMENTE'
+# Modo de uso: copia o mueve este script a /usr/bin o /usr/local/bin y desde el directorio donde se encuentre la copia de un repo git, ejecútalo de esta manera:
+# autogit <ficheros>
+
+# Comprobamos si el directorio en el que estamos es de un repositorio git
+if [ ! -d '.git' ]; then
+	echo 'Esta carpeta no contiene un repositorio Git'
+	exit -1
+fi
+
+# Ahora comprobamos si se le paso algun parametro
+if [ $# == 0 ]; then
+	echo "autogit: ¡Error! No se le a pasado ningún parámetro"
+	exit -1
+else
+	# Recorremos los parametros para comprobar si son ficheros o directorios
+	for file in $*; do
+		if [ ! -e $file ]; then
+			echo "UpToGit: El archivo o directorio $file no existe"
+			exit -1
+		fi
+	done
+	
+	# Si llegamos hasta aquí, indicamos a Git los archivos a subir
+	git add $*
+	
+	# Esto nos pedira el mensaje del commit
+	git commit -m "$DIA+$HORA+$TXT"
+
+	# Y terminamos subiendo los archivos
+	git push https://github.com/LBRD-grupo/laboratorio.git
+
+fi
+	
+
+fi
+	
 
 fi
 	
